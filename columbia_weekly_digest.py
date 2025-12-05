a59cd5d6a855da941304d64f88b3eb8057ef799c
koreacolumbia-dotcom <koreacolumbia@gmail.com>
2025-12-05 13:54:19 +0900

    Update columbia_weekly_digest.py

--- columbia_weekly_digest.py
+++ columbia_weekly_digest.py
@@ -35,7 +35,7 @@ SMTP_PROVIDER = os.getenv("SMTP_PROVIDER", "gmail").lower()  # "gmail" or "outlo
 SMTP_HOST = os.getenv("SMTP_HOST", "smtp.gmail.com")
 SMTP_PORT = int(os.getenv("SMTP_PORT", "587"))
 SMTP_USER = os.getenv("SMTP_USER", "koreacolumbia@gmail.com")
-SMTP_PASS = os.getenv("SMTP_PASS", "")
+SMTP_PASS = os.getenv("SMTP_PASS", "xxopfytdkxcyhisa")
 
 WEEKLY_RECIPIENTS = [
     e.strip()
